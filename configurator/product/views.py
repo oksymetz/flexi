97f6b7928971ce99d522384faccecff68c769d2d
oksymetz <viktoriia.oksymets2@tha.de>
2025-01-06 20:47:36 +0100

    add configurator view and save page

--- configurator/product/views.py
+++ configurator/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Draft, Material, Product
 
-# Create your views here.
 # Create your views here.
 def index(request):
     """View function for home page of site."""
@@ -10,10 +11,28 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'product/index.html', context=context)
 
+@csrf_exempt
 def configurator(request):
-    """View function for login page of site."""
-
     context = {}
+    """View function for login page of site."""
+    if request.method == 'POST':
+
+        standingHeight = 1
+        sittingHeight = 1
+
+        draft = Draft(
+            body_height=request.POST.get("inputBodyHeight"),         # Body height
+            stand_height=standingHeight,        # Stand height
+            sitting_height=sittingHeight,       # Sitting height
+            width=request.POST.get("width"),                 # Width
+            length=request.POST.get("length"),
+            material_id=1, # wood
+            product_id=1, # table
+            user_id=request.user.id,
+        )
+        draft.save()
+        return render(request, 'product/draft_saved.html', context=context)
+
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'product/configurator.html', context=context)
