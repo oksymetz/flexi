745750e5d0696cf6ab3fb900f6582bef8fc1c5d9
oksymetz <viktoriia.oksymets2@tha.de>
2025-01-06 20:51:14 +0100

    add drafts and log out urls

--- configurator/profiles/urls.py
+++ configurator/profiles/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('signin/', views.signin, name='signin'),
+    path('drafts/', views.drafts, name='drafts'),
+    path('logout/', views.log_out, name='log_out'),
 ]
