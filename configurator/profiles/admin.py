79d50a14225b6754bfe18a3efcd668afb359b36b
oksymetz <viktoriia.oksymets2@tha.de>
2025-01-06 20:49:27 +0100

    configure user model in db

--- configurator/profiles/admin.py
+++ configurator/profiles/admin.py
@@ -3,6 +3,6 @@ from .models import Customer
 
 # Register your models here.
 @admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('customer_id', 'name', 'email', 'created_at', 'updated_at')
+class Customer(admin.ModelAdmin):
+    list_display = ('id', 'address', 'created_at', 'updated_at')
 
